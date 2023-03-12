af688cc92d79b6f312d92f26d9bebecfe0389b69
Mohamed Emad <asd55asd36@gmail.com>
2023-03-12 03:07:30 +0200

    done

---
+++ app/src/main/java/com/example/programmercalculator/MainActivity.kt
@@ -0,0 +1,296 @@
+package com.example.programmercalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.programmercalculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var convertFrom = ""
+        var convertTo = ""
+
+        // convert from
+        binding.fromHexa.setOnClickListener {
+
+            convertFrom = "Hex"
+        }
+
+        binding.fromDec.setOnClickListener {
+
+            convertFrom = "Dec"
+        }
+
+        binding.fromBin.setOnClickListener {
+
+            convertFrom = "Bin"
+        }
+
+        binding.fromOct.setOnClickListener {
+
+            convertFrom = "Oct"
+        }
+
+        // convert to
+        binding.toHex.setOnClickListener {
+
+            convertTo = "Hex"
+        }
+
+        binding.toDec.setOnClickListener {
+
+            convertTo = "Dec"
+        }
+
+        binding.toOct.setOnClickListener {
+
+            convertTo = "Oct"
+        }
+
+        binding.toBin.setOnClickListener {
+
+            convertTo = "Bin"
+        }
+
+        binding.convert.setOnClickListener {
+
+            if (inputIsEmpty()) makeAlertToast()
+            else if (convertFrom.isEmpty() || convertTo.isEmpty()) Toast.makeText(this, "Select Operation", Toast.LENGTH_SHORT).show()
+            else {
+
+                when(convertFrom){
+
+                    "Hex" ->{
+
+                        if (checkIfInputIsValidForHex()){
+
+                            val input = binding.input.text.toString()
+                            when(convertTo){
+
+                                "Hex" -> {
+
+                                    binding.Answer.text = convertFromDecToHex(convertFromHexToDec(input)).uppercase()
+                                }
+
+                                "Dec" -> {
+
+                                    binding.Answer.text = convertFromDecToDec(convertFromHexToDec(input))
+                                }
+
+                                "Oct" -> {
+
+                                    binding.Answer.text = convertFromDecToOct(convertFromHexToDec(input))
+                                }
+
+                                "Bin" -> {
+
+                                    binding.Answer.text = convertFromDecToBin(convertFromHexToDec(input))
+                                }
+                            }
+
+                        } else makeAlertToast()
+
+                    }
+
+                    "Bin" ->{
+
+                        if (checkIfInputIsValidForBin()){
+
+                            val input = binding.input.text.toString()
+                            when(convertTo){
+
+                                "Hex" -> {
+
+                                    binding.Answer.text = convertFromDecToHex(convertFromBinToDec(input)).uppercase()
+                                }
+
+                                "Dec" -> {
+
+                                    binding.Answer.text = convertFromDecToDec(convertFromBinToDec(input))
+                                }
+
+                                "Oct" -> {
+
+                                    binding.Answer.text = convertFromDecToOct(convertFromBinToDec(input))
+                                }
+
+                                "Bin" -> {
+
+                                    binding.Answer.text = convertFromDecToBin(convertFromBinToDec(input))
+                                }
+                            }
+
+                        } else makeAlertToast()
+
+                    }
+
+                    "Dec" ->{
+
+                        if (checkIfInputIsValidForOctAndDec()){
+
+                            val input = binding.input.text.toString()
+                            when(convertTo){
+
+                                "Hex" -> {
+
+                                    binding.Answer.text = convertFromDecToHex(convertFromDecToDec(input)).uppercase()
+                                }
+
+                                "Dec" -> {
+
+                                    binding.Answer.text = convertFromDecToDec(convertFromDecToDec(input))
+                                }
+
+                                "Oct" -> {
+
+                                    binding.Answer.text = convertFromDecToOct(convertFromDecToDec(input))
+                                }
+
+                                "Bin" -> {
+
+                                    binding.Answer.text = convertFromDecToBin(convertFromDecToDec(input))
+                                }
+                            }
+
+                        } else makeAlertToast()
+
+                    }
+
+                    "Oct" ->{
+
+                        if (checkIfInputIsValidForOctAndDec()){
+
+                            val input = binding.input.text.toString()
+                            when(convertTo){
+
+                                "Hex" -> {
+
+                                    binding.Answer.text = convertFromDecToHex(convertFromOctToDec(input)).uppercase()
+                                }
+
+                                "Dec" -> {
+
+                                    binding.Answer.text = convertFromDecToDec(convertFromOctToDec(input))
+                                }
+
+                                "Oct" -> {
+
+                                    binding.Answer.text = convertFromDecToOct(convertFromOctToDec(input))
+                                }
+
+                                "Bin" -> {
+
+                                    binding.Answer.text = convertFromDecToBin(convertFromOctToDec(input))
+                                }
+                            }
+
+                        } else makeAlertToast()
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun makeAlertToast(){
+
+        Toast.makeText(this, "Invalid Input", Toast.LENGTH_SHORT).show()
+    }
+
+    // checking
+
+    private fun inputIsEmpty() : Boolean{
+
+        val input = binding.input.text.toString()
+        if (input == "") return true
+        return false
+    }
+
+    private fun checkIfInputIsValidForHex() : Boolean{
+
+        val input = binding.input.text.toString()
+        for (i in input){
+
+            if (i in '0'..'9' || i in 'a'..'f' || i in 'A'..'F')
+            else return false
+        }
+
+        return true
+    }
+
+    private fun checkIfInputIsValidForBin() : Boolean{
+
+        val input = binding.input.text.toString()
+        for (i in input){
+
+            if (i != '0' && i != '1') return false
+        }
+
+        return true
+    }
+
+    private fun checkIfInputIsValidForOctAndDec() : Boolean{
+
+        val input = binding.input.text.toString()
+        if (input[0] == '0') return false
+        for (i in input){
+
+            if (i in '0'..'9')
+            else return false
+        }
+
+        return true
+    }
+
+
+    // convert from * to decimal
+
+    private fun convertFromHexToDec(hex : String) : Int{
+
+        return hex.toInt(16)
+    }
+
+    private fun convertFromBinToDec(bin : String) : Int{
+
+        return bin.toInt(2)
+    }
+
+    private fun convertFromOctToDec(oct : String) : Int{
+
+        return oct.toInt(8)
+    }
+
+    private fun convertFromDecToDec(dec : String) : Int{
+
+        return dec.toInt()
+    }
+
+
+    // convert from decimal to *
+
+    private fun convertFromDecToBin(dec : Int) : String{
+
+        return Integer.toBinaryString(dec)
+    }
+
+    private fun convertFromDecToOct(dec : Int) : String{
+
+        return Integer.toOctalString(dec)
+    }
+
+    private fun convertFromDecToHex(dec : Int) : String{
+
+        return Integer.toHexString(dec)
+    }
+
+    private fun convertFromDecToDec(dec : Int) : String{
+
+        return dec.toString()
+    }
+}
